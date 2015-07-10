a68b2b0d7712527f7c9a582e1cf51980366ace85
Brandon Jeong <batsu05@gmail.com>
2015-07-10 16:40:24 +0900

    first draft

---
+++ photo/forms.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+from django import forms
+from photo.models import Photo
+
+class PhotoEditForm(forms.ModelForm):
+    class Meta:
+        model = Photo
+        exclude = ('filtered_image_file',)
+        #fields = ('image_file', 'description', )
---
+++ photo/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.core.urlresolvers import reverse_lazy
+from django.conf import settings
+
+# Create your models here.
+class Photo(models.Model):
+    image_file = models.ImageField(upload_to='%Y/%m/%d')
+    filtered_image_file = models.ImageField(upload_to='filterd/%Y/%m/%d')
+    description = models.TextField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    def delete(self, *args, **kwargs):
+        self.image_file.delete()
+        self.filtered_image_file.delete()
+        super(Photo, self).delete(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse_lazy('view_single_photo', kwargs={'photo_id':self.id})
